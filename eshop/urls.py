cd7db4f4cd0625b4e0826f70cca1072847489cc9
Kristiane Karine Kand <karine.kand@gmail.com>
2024-07-13 18:05:28 +0300

    Add urls, views and basic template

--- eshop/urls.py
+++ eshop/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import settings
+from .views import *
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
     path('', include('store.urls')),
+    path('', home, name='home'),
+    path('admin/', admin.site.urls),
+    path('contact/', contact, name='contact'),
+    path('about/', about, name='about'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
