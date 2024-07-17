93f211da0eb293b304138e53d8950721e4074f2d
Mihkel Pisa <mihkel.pisa@gmail.com>
2024-07-17 14:16:40 +0300

    Update models and admin.py

--- store/admin.py
+++ store/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from .models import Category, Product, Order
+from .models import Category, Product, Order, User, OrderItem, ShoppingCart, CartItem, Review
 
 admin.site.register(Category)
 admin.site.register(Product)
 admin.site.register(Order)
+admin.site.register(User)
+admin.site.register(OrderItem)
+admin.site.register(ShoppingCart)
+admin.site.register(CartItem)
+admin.site.register(Review)
+
