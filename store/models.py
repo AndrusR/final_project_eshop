18e391750b06bb2b33e5b13e98166b06d79a7b61
Mihkel Pisa <mihkel.pisa@gmail.com>
2024-07-20 12:36:09 +0300

    Adding further changes to forms.py and to views templates

--- store/models.py
+++ store/models.py
@@ -1,11 +1,10 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-import datetime
+from django.contrib.auth.models import User
+
 
 
 # Categories of Products
 class Category(models.Model):
-    category_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50) # Name of the category
 
     def __str__(self):
@@ -14,85 +13,47 @@ class Category(models.Model):
 
 # All of our Products
 class Product(models.Model):
-    product_id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=250)
+    name = models.CharField(max_length=250, help_text='Enter a name for the product')
     price = models.DecimalField(default=0, decimal_places=2, max_digits=10)
-    description = models.CharField(max_length=250, default='', blank=True, null=True)
-    image = models.ImageField(upload_to='uploads/product/')
+    description = models.CharField(max_length=250, default='', blank=True, null=True, help_text='Enter a description of the product')
+    image = models.ImageField(upload_to='media/product/')
     stock_quantity = models.IntegerField(default=0)
-    category_id = models.ForeignKey(Category, on_delete=models.CASCADE) # Links the product to a category
+    category = models.ForeignKey(Category, on_delete=models.CASCADE) # Links the product to a category
 
     def __str__(self):
         return self.name
 
 
-# Categories of User
-class User(models.Model):
-    user_id = models.AutoField(primary_key=True)
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=100)
-    is_staff = models.BooleanField(default=False) # Allows the user to access the admin site.
-    last_login = models.DateTimeField(blank=True, null=True) # A datetime of the user’s last login.
-
-    def __str__(self):
-        return f'{self.first_name} {self.last_name}'
-
-
 # Order represents the ready-to-purchase details by a user
 class Order(models.Model):
-    order_id = models.AutoField(primary_key=True)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE) # ForeignKey linking to the user who made the order
-    order_date = models.DateField(auto_now_add=True)
-    total_amount = models.DecimalField(default=0, decimal_places=2, max_digits=10)
-    status = models.BooleanField(default=False) # Status of the order, whether completed, pending etc.
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1, null=True) # ForeignKey linking to the user who made the order
+    number = models.CharField(max_length=50)
+    date = models.DateField(auto_now_add=True)
+    status = models.CharField() # Status of the order, whether completed, pending etc.
+    shopping_cart = models.ForeignKey("ShoppingCart", on_delete=models.CASCADE)
 
     def __str__(self):
-        return f'Order {self.order_id} is {self.status}.'
-
-
-# OrderItem represents a specific product added in an order
-class OrderItem(models.Model):
-    order_item_id = models.AutoField(primary_key=True)
-    order_id = models.ForeignKey(Order, on_delete=models.CASCADE) # ForeignKey linking to the order
-    product_id = models.ForeignKey(Product, on_delete=models.CASCADE) # ForeignKey linking to the product
-    quantity = models.IntegerField(default=1)
-    price = models.DecimalField(default=0, decimal_places=2, max_digits=10)
-
-    def __str__(self):
-        return f'{self.product_id} x {self.quantity}'
+        return f'Order {self.number} is {self.status}.'
 
 
 # ShoppingCart represents a collection of products that a user intends to buy
 class ShoppingCart(models.Model):
-    shopping_cart_id = models.AutoField(primary_key=True)
-    user_id = models.OneToOneField(User, on_delete=models.CASCADE)
-
-    def __str__(self, shopping_cart_id=None):
-        return f'Shopping cart {shopping_cart_id} for {self.user_id}'
-
-
-# CartItem represents a specific product added to a ShoppingCart
-class CartItem(models.Model):
-    cart_item_id = models.AutoField(primary_key=True)
-    shopping_cart_id = models.ForeignKey(ShoppingCart, on_delete=models.CASCADE)
-    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.IntegerField(default=1)
+    total_price = models.DecimalField(default=0, decimal_places=2, max_digits=10)
 
     def __str__(self):
-        return f'{self.product_id} x {self.quantity}'
+        return f'Shopping cart {self.id}.'
 
 
 # Review represents feedback by a user about a product
 class Review(models.Model):
-    review_id = models.AutoField(primary_key=True)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
     rating = models.IntegerField(default=0) # Rating given by the user
     comment = models.TextField() # Comment added by the user
-    review_date = models.DateField(auto_now_add=True)
+    date = models.DateField(auto_now_add=True)
 
     def __str__(self):
-        return f'Review on {self.product_id} by {self.user_id} on {self.review_date}.'
+        return f'Review on {self.product} by {self.user} on {self.date}.'
 
