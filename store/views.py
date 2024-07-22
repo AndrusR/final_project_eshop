3e05c7cbefc280fe3ceb5cdc808368ae86a7dc9d
Mihkel Pisa <mihkel.pisa@gmail.com>
2024-07-22 19:33:36 +0300

    Changes to sidebar.html

--- store/views.py
+++ store/views.py
@@ -36,40 +36,12 @@ def contact(request):
 # Product Views
 def product_list(request):
     products = Product.objects.all()
-    return render(request, 'views templates/product_list.html', {'products': products})
+    return render(request, 'product_list.html', {'products': products})
 
 
 def product_detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
-    return render(request, 'views templates/product_detail.html', {'product': product})
+    return render(request, 'product_detail.html', {'product': product})
 
 
-def product_create(request):
-    if request.method == 'POST':
-        form = ProductForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect('product-list')
-    else:
-        form = ProductForm()
-    return render(request, 'views templates/product_form.html', {'form': form})
 
-
-def product_update(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    if request.method == 'POST':
-        form = ProductForm(request.POST, request.FILES, instance=product)
-        if form.is_valid():
-            form.save()
-            return redirect('product-list')
-    else:
-        form = ProductForm(instance=product)
-    return render(request, 'views templates/product_form.html', {'form': form})
-
-
-def product_delete(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    if request.method == 'POST':
-        product.delete()
-        return redirect('product-list')
-    return render(request, 'views templates/product_confirm_delete.html', {'product': product})
