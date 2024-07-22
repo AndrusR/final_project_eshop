b42582313ece412b1b5d3ff536a3efa3449b0703
Mihkel Pisa <mihkel.pisa@gmail.com>
2024-07-22 19:33:36 +0300

    Changes to sidebar.html

--- store/urls.py
+++ store/urls.py
@@ -7,14 +7,10 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
 
-    path('category/<str:category_name>', views.category, name="category"),
-
     path('product/<int:pk>/', views.product, name="product"),
 
 
     path('products/', views.product_list, name='product-list'),
     path('products/<int:pk>/', views.product_detail, name='product-detail'),
-    path('products/new/', views.product_create, name='product-create'),
-    path('products/<int:pk>/edit/', views.product_update, name='product-update'),
-    path('products/<int:pk>/delete/', views.product_delete, name='product-delete'),
+
 ]
